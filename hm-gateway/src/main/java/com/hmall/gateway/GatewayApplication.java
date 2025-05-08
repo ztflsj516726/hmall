8224e5b7c2c1a8f4949dac71e2639807cecccc1a
养乐多lsj <11275955+yakult-lsj@user.noreply.gitee.com>
2025-05-08 17:16:17 +0800

    网关

---
+++ hm-gateway/src/main/java/com/hmall/gateway/GatewayApplication.java
@@ -0,0 +1,20 @@
+package com.hmall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ClassName:GatewayApplication
+ * Package:IntelliJ IDEA
+ * Description:
+ *
+ * @Author ztf
+ * @Create 2025/5/8-16:28
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String [] args){
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
